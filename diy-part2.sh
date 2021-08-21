4fb2fced9bfe85b15217faf3f71157e82c384951
pzcn <sdustpedro@gmail.com>
2021-08-21 13:17:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/.140/.132/g' include/kernel-version.mk
+sed -i 's/2c657a2df4f2e805ac50aeb39ecf5d1e4cb1501e7fcf15eafd14486b587f7e43/8466adbfb3579e751ede683496df7bb20f258b5f882250f3dd82be63736d00ef/g' include/kernel-version.mk
