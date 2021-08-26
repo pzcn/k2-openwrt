87050ab42b283bcd3aee3acad86711af7874da58
pzcn <sdustpedro@gmail.com>
2021-08-26 22:27:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i 's/.140/.113/g' include/kernel-version.mk
-sed -i 's/2c657a2df4f2e805ac50aeb39ecf5d1e4cb1501e7fcf15eafd14486b587f7e43/30cde92463c474b75f9eb197654570dd6dbb32ec20695544c5469f292662da47/g' include/kernel-version.mk
-curl -o target/linux/generic/pending-5.4/690-net-add-support-for-threaded-NAPI-polling.patch https://raw.githubusercontent.com/coolsnowwolf/lede/5dcbd82a57966f98fc814f9a21dfa73f7c6d41b5/target/linux/generic/pending-5.4/690-net-add-support-for-threaded-NAPI-polling.patch
+sed -i 's/.142/.115/g' include/kernel-version.mk
+sed -i 's/99785728968564ba27c7e552d024b560072dcbc885540912eabb5c021e231451/5b4724748527a622a886232b5da1471619eb3aa3bb38a11851275eba09989dd2/g' include/kernel-version.mk
+curl -o target/linux/generic/pending-5.4/690-net-add-support-for-threaded-NAPI-polling.patch https://raw.githubusercontent.com/coolsnowwolf/lede/376ec4b6ed491a0f4060a68b10ee733d1a509556/target/linux/generic/pending-5.4/690-net-add-support-for-threaded-NAPI-polling.patch
