8c2451aa8ae541106e8ca6018e94c842174a3ef5
pzcn <sdustpedro@gmail.com>
2021-08-27 08:10:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,4 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 sed -i 's/.142/.115/g' include/kernel-version.mk
 sed -i 's/99785728968564ba27c7e552d024b560072dcbc885540912eabb5c021e231451/5b4724748527a622a886232b5da1471619eb3aa3bb38a11851275eba09989dd2/g' include/kernel-version.mk
 curl -o target/linux/generic/pending-5.4/690-net-add-support-for-threaded-NAPI-polling.patch https://raw.githubusercontent.com/coolsnowwolf/lede/376ec4b6ed491a0f4060a68b10ee733d1a509556/target/linux/generic/pending-5.4/690-net-add-support-for-threaded-NAPI-polling.patch
+curl -o target/linux/generic/hack-5.4/662-remove_pfifo_fast.patch https://raw.githubusercontent.com/coolsnowwolf/lede/707e6ed1a19db0a12f553cafbd5851d3591420af/target/linux/generic/hack-5.4/662-remove_pfifo_fast.patch
